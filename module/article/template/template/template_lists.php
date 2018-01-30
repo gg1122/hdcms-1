dbdbd4c7ec4b7939b780863d0fe8a849ffedb0ac
houdunwang <2300071698@qq.com>
2018-01-30 19:08:43 +0800

    修复卸载模板失败的问题

--- module/article/template/template/template_lists.php
+++ module/article/template/template/template_lists.php
@@ -53,7 +53,7 @@
                 <foreach from="$data" value="$d">
                     <div class="thumbnail action">
                         <h5>{{$d['title']}}</h5>
-                        <img src="theme/{{$d['name']}}/{{$d['thumb']}}">
+                        <img src="{!!$d['thumb']!!}">
 <!--                        <div class="caption">-->
 <!--                            <button type="button" class="btn btn-default btn-xs btn-block">使用</button>-->
 <!--                        </div>-->
--- version.php
+++ version.php
@@ -1,6 +1,6 @@
 <?php return array (
-  'version' => 'v2.131',
-  'build' => '20180130154336',
+  'version' => 'v2.132',
+  'build' => '20180130190734',
   'logs' => '',
   'type' => 'upgrade',
   'explain' => '',
